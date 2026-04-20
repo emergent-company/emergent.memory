2ceccf7cf7dff6877360adb933e458d5697090e2
Maciej Kucharz <maciej.kucharz@eyedea.io>
2026-04-20 16:46:21 +0200

    remove codebase CLI from emergent.memory
    CLI moved to its proper home: github.com/mkucharz/code-memory-blueprint
    Released there as v1.0.0. Workflow removed from this repo.

--- blueprints/code-memory-blueprint/cmd/codebase/internal/branch/cmd.go
+++
@@ -1,9 +0,0 @@
-package branchcmd
-
-import "github.com/spf13/cobra"
-
-func NewCmd(flagProjectID *string) *cobra.Command {
-	cmd := &cobra.Command{Use: "branch", Short: "Graph branch operations"}
-	cmd.AddCommand(newVerifyCmd(flagProjectID))
-	return cmd
-}
