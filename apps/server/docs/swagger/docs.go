f27c0fa4a91eb1642d7d1df0cdb286a03f1a3da2
Maciej Kucharz <maciej.kucharz@eyedea.io>
2026-05-03 23:56:49 +0200

    chore: bump version to 0.40.68

--- apps/server/docs/swagger/docs.go
+++ apps/server/docs/swagger/docs.go
@@ -27592,7 +27592,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "0.40.67",
+	Version:          "0.40.68",
 	Host:             "localhost:5300",
 	BasePath:         "/",
 	Schemes:          []string{"http", "https"},
